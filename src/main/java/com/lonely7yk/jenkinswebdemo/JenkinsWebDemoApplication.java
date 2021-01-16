56d50fde3eebb38d7cc23bca6b377f24c08b4cd3
lonely7yk <893077810@qq.com>
2021-01-15 19:52:20 -0800

    first

---
+++ src/main/java/com/lonely7yk/jenkinswebdemo/JenkinsWebDemoApplication.java
@@ -0,0 +1,13 @@
+package com.lonely7yk.jenkinswebdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsWebDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsWebDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lonely7yk/jenkinswebdemo/JenkinsWebDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lonely7yk.jenkinswebdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsWebDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
