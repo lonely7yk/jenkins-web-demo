7561b078981c51e8292285c93442236d47494b21
lonely7yk <893077810@qq.com>
2021-01-23 01:49:59 -0800

    add branch name in each API in HelloController

--- src/main/java/com/lonely7yk/jenkinswebdemo/controller/HelloController.java
+++ src/main/java/com/lonely7yk/jenkinswebdemo/controller/HelloController.java
@@ -5,28 +5,30 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+	public static final String BRANCH = "master";
+
 	@RequestMapping("/hello")
 	public String hello() {
-		return "hello";
+		return BRANCH + ": hello";
 	}
 
 	@RequestMapping("/hello/add")
 	public String add() {
-		return "hello add";
+		return BRANCH + ": hello add";
 	}
 
 	@RequestMapping("/hello/update")
 	public String update() {
-		return "hello update";
+		return BRANCH + ": hello update";
 	}
 
 	@RequestMapping("/hello/delete")
 	public String delete() {
-		return "hello delete";
+		return BRANCH + ": hello delete";
 	}
 
 	@RequestMapping("/hello/get")
 	public String get() {
-		return "hello get";
+		return BRANCH + ": hello get";
 	}
 }
