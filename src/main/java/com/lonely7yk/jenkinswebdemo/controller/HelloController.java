662e5316db4b189a5a2da17a2f1cc737e9ddc658
lonely7yk <893077810@qq.com>
2021-01-23 01:51:08 -0800

    change branch name from master to v1

--- src/main/java/com/lonely7yk/jenkinswebdemo/controller/HelloController.java
+++ src/main/java/com/lonely7yk/jenkinswebdemo/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-	public static final String BRANCH = "master";
+	public static final String BRANCH = "v1";
 
 	@RequestMapping("/hello")
 	public String hello() {
